7cdf299b0fade603da046e0f272662a987bcdcc1
misnosugianto48 <misnosugianto48@gmail.com>
2023-07-28 22:27:43 +0700

    users registration imlemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,8 +1,8 @@
-const { Pool } = require("pg");
-const { nanoid } = require("nanoid");
-const InvariantError = require("../../exception/InvariantError");
-const { mapDBToModel } = require("../../utils");
-const NotFoundError = require("../../exception/NotFoundError");
+const { Pool } = require('pg');
+const { nanoid } = require('nanoid');
+const InvariantError = require('../../exception/InvariantError');
+const { mapDBToModel } = require('../../utils');
+const NotFoundError = require('../../exception/NotFoundError');
 
 class NotesService {
   constructor() {
@@ -15,31 +15,31 @@ class NotesService {
     const updatedAt = createdAt;
 
     const query = {
-      text: "INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id",
+      text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
       values: [id, title, body, tags, createdAt, updatedAt],
     };
 
     const result = await this._pool.query(query);
 
     if (!result.rows[0].id) {
-      throw new InvariantError("Catatan gagal ditambahkan");
+      throw new InvariantError('Catatan gagal ditambahkan');
     }
 
     return result.rows[0].id;
   }
   async getNotes() {
-    const result = await this._pool.query("SELECT * FROM notes");
+    const result = await this._pool.query('SELECT * FROM notes');
     return result.rows.map(mapDBToModel);
   }
   async getNoteById(id) {
     const query = {
-      text: "SELECT * FROM notes WHERE id = $1",
+      text: 'SELECT * FROM notes WHERE id = $1',
       values: [id],
     };
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
-      throw new NotFoundError("Catatan tidak ditemukan");
+      throw new NotFoundError('Catatan tidak ditemukan');
     }
 
     return result.rows.map(mapDBToModel)[0];
@@ -47,26 +47,27 @@ class NotesService {
   async editNoteById(id, { title, body, tags }) {
     const updatedAt = new Date().toISOString();
     const query = {
-      text: "UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id",
+      text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
       values: [title, body, tags, updatedAt, id],
     };
 
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
-      throw new NotFoundError("Gagal memperbarui catatan. Id tidak ditemukan");
+      throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
     }
   }
+
   async deleteNoteById(id) {
     const query = {
-      text: "DELETE FROM notes WHERE id = $1 RETURNING id",
+      text: 'DELETE FROM notes WHERE id = $1 RETURNING id',
       values: [id],
     };
 
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
-      throw new NotFoundError("Catatan gagal dihapus. Id tidak ditemukan");
+      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
     }
   }
 }
